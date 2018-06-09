b656833bfdeba3775badca8d5199ccc294701146
MarioHurtadoLopezING <marioolopez21@gmail.com>
2018-06-09 13:00:35 -0500

    Servicios rest
    Se crean los servicios rest de las entidades de bae de datos.

---
+++ Servicios web/src/servicios/ApplicationConfig.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servicios;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Desktop
+ */
+@javax.ws.rs.ApplicationPath("webresources")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(servicios.AlbumFacadeREST.class);
+        resources.add(servicios.ArtistaFacadeREST.class);
+        resources.add(servicios.CancionFacadeREST.class);
+        resources.add(servicios.CancionListaFacadeREST.class);
+        resources.add(servicios.CancionprivadaFacadeREST.class);
+        resources.add(servicios.GeneroArtistaFacadeREST.class);
+        resources.add(servicios.HistorialreproduccionFacadeREST.class);
+        resources.add(servicios.ListareproduccionFacadeREST.class);
+        resources.add(servicios.UsuarioFacadeREST.class);
+    }
+    
+}
