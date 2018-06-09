8e6ef04a2fb2abdd55eea050cdbbeba14c7dde23
Renato <renaswat@gmail.com>
2018-06-09 12:48:41 -0500

    Correcciones
    Se realizaron correciones a las ventanas

--- Cliente_Musica/Cliente_Musica/src/controlador/VentanaMenuPrincipalController.java
+++ Cliente_Musica/Cliente_Musica/src/controlador/VentanaMenuPrincipalController.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controlador;
 
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,7 +8,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -22,7 +15,6 @@ import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.controlsfx.control.PopOver;
@@ -93,21 +85,18 @@ public class VentanaMenuPrincipalController implements Initializable {
                     pop.hide();
                     panelPrincipal.getChildren().clear();
                     panelPrincipal.setCenter(root);
+                    
                 } catch (IOException ex) {
                     Logger.getLogger(VentanaMenuPrincipalController.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
         });
         
-       
-        
         box.getChildren().addAll(botonBibliotecaPublica, botonBibliotecaPrivada);
         pop.setContentNode(box);
         pop.setArrowLocation(PopOver.ArrowLocation.LEFT_CENTER);
         pop.show(botonBibliotecas);
     }
-
-
 
     @FXML
     private void desplegarPanelSubirCancion(ActionEvent event) throws IOException {
@@ -142,7 +131,4 @@ public class VentanaMenuPrincipalController implements Initializable {
         panelPrincipal.getChildren().clear();
         panelPrincipal.setCenter(root);
     }
-
-
-    
 }
