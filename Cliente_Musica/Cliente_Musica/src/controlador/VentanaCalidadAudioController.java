59c45431371d9451f0db79b36138c281284cd2e4
MarioHurtadoLopezING <marioolopez21@gmail.com>
2018-06-14 15:01:15 -0500

    Caso de uso seleccionar calidad de audio terminado
    Se implementa el caso de uso seleccionar calidad de audio

--- Cliente_Musica/Cliente_Musica/src/controlador/VentanaCalidadAudioController.java
+++ Cliente_Musica/Cliente_Musica/src/controlador/VentanaCalidadAudioController.java
@@ -1,9 +1,19 @@
-
 package controlador;
 
+import InterfazGrafica.MessageFactory;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
 /**
  * FXML Controller class
@@ -12,12 +22,53 @@ import javafx.fxml.Initializable;
  */
 public class VentanaCalidadAudioController implements Initializable {
 
+    @FXML
+    private RadioButton btnBajo;
+    @FXML
+    private RadioButton btnMedio;
+    @FXML
+    private RadioButton btnAlto;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+        ToggleGroup group = new ToggleGroup();
+        btnMedio.setToggleGroup(group);
+        btnBajo.setToggleGroup(group);
+        btnAlto.setToggleGroup(group);
+    }
+
+    public void guardarCalidad() {
+        FileWriter fw = null;
+        try {
+            String calidad = "";
+            if (btnBajo.isSelected()) {
+                calidad = "bajo";
+            } else if (this.btnMedio.isSelected()) {
+                calidad = "medio";
+            } else if (this.btnAlto.isSelected()) {
+                calidad = "alto";
+            }   String ruta = "C:\\calidadAudio";
+            File directorio = new File(ruta);
+            if (!directorio.exists()) {
+                directorio.mkdir();
+            }   File archivo = new File(directorio + "\\calidad.txt");
+            fw = new FileWriter(archivo);
+            PrintWriter pw = new PrintWriter(fw);
+            System.out.println("Guardando calidad");
+            MessageFactory.showMessage("Calidad seleccionada", "Calidad cambiada","La calidad seleccionada es "+calidad, Alert.AlertType.INFORMATION);
+            pw.println(calidad);
+        } catch (IOException ex) {
+            Logger.getLogger(VentanaCalidadAudioController.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                fw.close();
+            } catch (IOException ex) {
+                Logger.getLogger(VentanaCalidadAudioController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
 }
